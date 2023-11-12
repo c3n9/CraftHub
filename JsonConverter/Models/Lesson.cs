1b509770a7b672caf360d626d37a81c8ecd32aaf
c3n9 <108518693+c3n9@users.noreply.github.com>
2023-11-13 00:50:13 +0300

    import and export json file

---
+++ JsonConverter/Models/Lesson.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonConverter.Models
+{
+    public partial class Lesson
+    {
+        public bool IsEnabled { get; set; }
+        public int Level { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsSimulatorMode { get; set; }
+        public int LessonBlockId { get; set; }
+        public int LessonNumber { get; set; }
+        public int PagesCount { get; set; }
+        public bool IsCommon { get; set; }
+        public string RobotPrefab { get; set; }
+        public string FieldPrefab { get; set; }
+
+    }
+}
---
+++ JsonConverter/Pages/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using JsonConverter.Models;
+using Microsoft.Win32;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JsonConverter.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPage.xaml
+    /// </summary>
+    public partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void BImport_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            if (dialog.ShowDialog().GetValueOrDefault())
+            {
+                DataManager.jsonFile = File.ReadAllText(dialog.FileName);   
+            }
+            Refresh();
+        }
+
+        private void Refresh()
+        {
+            DGLessons.ItemsSource = DataManager.Lessons.ToList();
+        }
+
+        private void BExport_Click(object sender, RoutedEventArgs e)
+        {
+            var jsonData = JsonConvert.SerializeObject(DataManager.Lessons);
+            var dialog = new SaveFileDialog() { Filter = ".json | *.json"};
+            if(dialog.ShowDialog().GetValueOrDefault())
+            {
+                File.WriteAllText(dialog.FileName, jsonData, Encoding.UTF8);
+            }
+        }
+    }
+}
