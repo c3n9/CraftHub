b831432c6e2862f22b8f59cc842069572618f867
c3n9 <108518693+c3n9@users.noreply.github.com>
2023-12-22 21:35:28 +0300

    global validation

--- JsonConverter/App.xaml.cs
+++ JsonConverter/App.xaml.cs
@@ -13,5 +13,15 @@ namespace JsonConverter
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MessageBox.Show(e.Exception.Message);
+        }
     }
 }
