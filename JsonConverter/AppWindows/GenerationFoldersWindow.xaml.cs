483c4f21aa476d8e5bf4b22db926131cb1d82cd1
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-01-02 01:28:44 +0300

    rewrote the method for generating folders

--- JsonConverter/AppWindows/GenerationFoldersWindow.xaml.cs
+++ JsonConverter/AppWindows/GenerationFoldersWindow.xaml.cs
@@ -25,8 +25,8 @@ namespace JsonConverter.AppWindows
     /// </summary>
     public partial class GenerationFoldersWindow : Window
     {
-        string folderPath;
-        string notCommonFolderPath;
+        //string folderPath;
+        //string notCommonFolderPath;
         List<string> folders = new List<string>() { "Python", "Java", "LabView" };
         public GenerationFoldersWindow()
         {
@@ -61,7 +61,7 @@ namespace JsonConverter.AppWindows
                 for (int i = 1; i <= lessonCount; i++)
                 {
                     string folderName = $"{level[0]}{blockNumber}_{i}";
-                    folderPath = System.IO.Path.Combine(selectedPath, folderName);
+                    string folderPath = System.IO.Path.Combine(selectedPath, folderName);
 
                     // Проверяем, существует ли папка, прежде чем создавать
                     if (!Directory.Exists(folderPath))
@@ -69,15 +69,15 @@ namespace JsonConverter.AppWindows
                         Directory.CreateDirectory(folderPath);
                         if (IsCommon.IsChecked.Value)
                         {
-                            GenreateCommonLesson();
+                            GenerateLesson(folderPath, folderName);
                         }
                         else
                         {
                             foreach (string folder in folders)
                             {
-                                notCommonFolderPath = System.IO.Path.Combine(folderPath, folder.ToString());
+                                string notCommonFolderPath = System.IO.Path.Combine(folderPath, folder.ToString());
                                 Directory.CreateDirectory(notCommonFolderPath);
-                                GenerateNotCommonLesson();
+                                GenerateLesson(notCommonFolderPath, folder);
                             }
                         }
 
@@ -86,34 +86,9 @@ namespace JsonConverter.AppWindows
                 }
             }
         }
-
-        private void GenerateNotCommonLesson()
+        private void GenerateLesson(string folderPath, string folderName)
         {
-            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true, Filter = "*.png, *.jpg; | *.png; *.jpg;" };
-            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                int countPage = 1;
-                foreach (var file in openFileDialog.FileNames)
-                {
-                    string destinationImagePath = System.IO.Path.Combine(notCommonFolderPath, $"Page{countPage}" + System.IO.Path.GetExtension(file));
-                    File.Copy(file, destinationImagePath);
-                    // Добавление метаинформации для Unity
-                    string metaFilePath = System.IO.Path.Combine(notCommonFolderPath, $"Page{countPage}" + System.IO.Path.GetExtension(file) + ".meta");
-                    var resourceStream = Application.GetResourceStream(new Uri("Recourses/template.meta", UriKind.Relative));
-                    using (var reader = new StreamReader(resourceStream.Stream))
-                    {
-                        string content = reader.ReadToEnd();
-                        File.WriteAllText(metaFilePath, content);
-                    }
-                    countPage++;
-
-                }
-            }
-        }
-
-        private void GenreateCommonLesson()
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true, Filter = "*.png, *.jpg; | *.png; *.jpg;" };
+            OpenFileDialog openFileDialog = new OpenFileDialog() { Multiselect = true, Filter = $"Uploading to {folderName}| *.png; *.jpg;" };
             if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 int countPage = 1;
