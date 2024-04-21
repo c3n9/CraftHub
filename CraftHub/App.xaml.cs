c2046925d9b6ff7e22f70e6c9a33117c312ca335
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-04-21 12:14:19 +0300

    update to .net 8.0

--- CraftHub/App.xaml.cs
+++ CraftHub/App.xaml.cs
@@ -1,19 +1,16 @@
 ﻿using CraftHub.ViewModels;
-using System;
-using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace CraftHub
 {
     /// <summary>
-    /// Логика взаимодействия для App.xaml
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
         internal static MainWindowViewModel mainWindowViewModel;
     }
+
 }
--- CraftHub/Views/MainWindow.xaml.cs
+++ CraftHub/Views/MainWindow.xaml.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -18,7 +14,7 @@ using System.Windows.Shapes;
 namespace CraftHub.Views
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
