5ea82cedb5f485958d94b20b401ad9cc23a834b9
c3n9 <108518693+c3n9@users.noreply.github.com>
2024-04-21 16:48:31 +0300

    some changes

--- CraftHub/ViewModels/WorkingWithJsonViewModel.cs
+++ CraftHub/ViewModels/WorkingWithJsonViewModel.cs
@@ -1,24 +1,54 @@
-﻿using CraftHub.ViewModels.Commands;
+﻿using CraftHub.Models;
+using CraftHub.ViewModels.Base;
+using CraftHub.ViewModels.Commands;
 using CraftHub.Views;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Input;
 
 namespace CraftHub.ViewModels
 {
-    internal class WorkingWithJsonViewModel
+    internal class WorkingWithJsonViewModel : BaseViewModel
     {
         public ICommand NavigateToPropertiesViewCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand RemoveCommand { get; set; }
+        public ICommand ExportCommand { get; set; }
+
         public WorkingWithJsonViewModel()
         {
-            NavigateToPropertiesViewCommand = new DelegateCommand(OnNavigateToPropertiesViewCommand);
+            NavigateToPropertiesViewCommand = new DelegateCommand(NavigateToPropertiesView);
+            AddCommand = new DelegateCommand(OnAddCommand);
+            EditCommand = new DelegateCommand(OnEditCommand);
+            RemoveCommand = new DelegateCommand(OnRemoveCammand);
+            ExportCommand = new DelegateCommand(OnExportCommand);
         }
-        private void OnNavigateToPropertiesViewCommand(object parameter)
+
+        private void NavigateToPropertiesView(object parameter)
         {
             App.mainWindowViewModel.MainFrameSource = new PropertiesView();
+        }
+
+        private void OnAddCommand(object parameter)
+        {
+
+        }
+
+        private void OnEditCommand(object parameter)
+        {
+
+        }
+
+        private void OnRemoveCammand(object parameter)
+        {
+
+        }
+
+        private void OnExportCommand(object parameter)
+        {
+
         }
     }
 }
